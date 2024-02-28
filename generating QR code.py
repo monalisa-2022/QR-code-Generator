649e45e93c03a97d3ee1cc793c037b63ed71562a
monalisa-2022 <157516945+monalisa-2022@users.noreply.github.com>
2024-02-28 23:13:13 +0530

    Update generating QR code.py

--- generating QR code.py
+++ generating QR code.py
@@ -1,3 +1,8 @@
-import qrcode as qr
-img=qr.make("http://saswatkumar.com/")
-img.save("SAP.png")
+import qrcode
+from PIL import Image
+qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=10,border=4)
+link=input("enter url")
+qr.add_data("link")
+qr.make(fit=True)
+img=qr.make_image(fill_color="black",back_color="white")
+img.save("monalisa.jpg")
